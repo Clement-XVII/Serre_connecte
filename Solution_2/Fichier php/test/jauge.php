c66230da298e33a49d17ccb0f0c0f474e6064840
Orion_XVII <76408216+Clement-XVII@users.noreply.github.com>
2021-06-01 17:07:10 +0200

    Update jauge.php

--- Solution_2/Fichier php/test/jauge.php
+++ Solution_2/Fichier php/test/jauge.php
@@ -3,20 +3,21 @@
     header("Refresh: 5; URL=$url");
     /* Database connection settings */
     $servername = "localhost";
-    $username = "root";
-    $password = "";
+    $username = "clement";
+    $password = "1234";
     $dbname = "test";
     $conn = mysqli_connect($servername, $username, $password, $dbname);
-    
+
 
     $time = '';
-    $value = '';
+    $temp = '';
+    $hum = '';
     $today = date("Y-m-d H:i:s");
     $tommorow = date('Y-m-d H:i:s', strtotime($today . ' +1 day'));
 
     //query to get data from the table
     //'2021-04-13 00:00:00'
-    $sql = "SELECT time, value FROM sensor ORDER BY id DESC";
+    $sql = "SELECT time, temp,hum FROM sensor ORDER BY id DESC";
     //$sql = "SELECT * FROM sensor WHERE id BETWEEN 50 AND 100";
     //"SELECT order_date, SUM(grand_total) AS total_grand  FROM orders GROUP BY order_date LIMIT = 15";
     $result = mysqli_query($conn, $sql);
@@ -25,11 +26,13 @@
     while ($row = mysqli_fetch_array($result)) {
 
         $time = $time . '"'. $row['time'].'",';
-        $value = $value . '"'. $row['value'] .'",';
+        $temp = $temp . '"'. $row['temp'] .'",';
+        $hum = $hum . '"'. $row['hum'] .'",';
     }
 
     $time = trim($time,",");
-    $value = trim($value,",");
+    $temp = trim($temp,",");
+    $hum = trim($hum,",");
 ?>
 <!doctype html>
 <html>
@@ -37,7 +40,7 @@
   <meta charset="utf-8"/>
   <meta name="author" content="Clement-XVII">
   <title>Custom Sectors</title>
-  <style type="text/css">     
+  <style type="text/css">
   body{
     font-family: Arial;
       margin: 80px 100px 10px 100px;
@@ -74,10 +77,10 @@
     box-shadow: 0 9px #999;
   }
 
-  .button:hover {background-color: red}
+  .button:hover {background-color: #3072ab}
 
   .button:active {
-    background-color: red;
+    background-color: #3072ab;
     box-shadow: 0 5px #666;
     transform: translateY(4px);
   }
@@ -87,6 +90,7 @@
     <h1>Dernière température sous forme de jauge exprimer en degrès celsius</h1>
   <div class="container">
     <div id="g" class="gauge"></div>
+    <div id="g1" class="gauge"></div>
     </div>
   <script src="raphael-2.1.4.min.js"></script>
   <script src="justgage.js"></script>
@@ -95,7 +99,7 @@
     var g = new JustGage({
       id: "g",
       label: "température",
-      value : [<?php echo $value; ?>],
+      value : [<?php echo $temp; ?>],
       symbol: ' °C',
       valueFontColor: "white",
       min: 0,
@@ -126,13 +130,46 @@
         }],
       });
 
-  
+    var g1 = new JustGage({
+      id: "g1",
+      label: "Humidité",
+      value : [<?php echo $hum; ?>],
+      symbol: ' %',
+      valueFontColor: "white",
+      min: 0,
+      max: 100,
+      decimals: 2,
+      gaugeWidthScale: 0.6,
+      pointer: true,
+      customSectors: [{
+        color : "#ff0000",
+          lo : 0,
+          hi : 30
+        },{
+          color : "#ffff00",
+          lo : 30,
+          hi : 40
+         },{
+          color : "#00ff00",
+          lo : 40,
+          hi : 70
+         },{
+          color : "#ffff00",
+          lo : 70,
+          hi : 80
+         },{
+          color : "#ff0000",
+          lo : 80,
+          hi : 100
+        }],
+      });
+
+
   </script>
   </body>
   <form>
   <button class=button type="button" onclick="document.location='http://localhost/test/datenter.php'">Tableau</button>
   <button class=button type="button" onclick="document.location='http://localhost/test/index.html'">Accueil</button>
   <button class=button type="button" onclick="document.location='http://localhost/test/graph.php'">Graphique</button>
-    
     </form>
 </html>
